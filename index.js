75f6e580d19a575c0b37c377115b262919eba2a0
Nivedhitha <nive.mathan21@gmail.com>
2024-06-02 10:41:49 -0400

    update cards

--- index.js
+++ index.js
@@ -98,16 +98,15 @@ let pageScrolled = false;
 
 const data = {
   [projectNames.CIRCLE]: {
-    title: "Social Balance through App Design",
+    title: "Achieving Social Balance",
     keyText: `We developed high-fidelity functional prototypes and tested them.`,
     icon: "./assets/projects/CIRCLE.svg",
-    highlightedIcon: "./assets/projects-highlighted/CIRCLE.svg",
+    sameSizeIcon: "./assets/projects-same-size/CIRCLE.svg",
     imageAlt: "Description 2",
     problemText: `Our aim was to develop a solution to help introverts maintain a
-    balance between social and personal well-being, as part of our
-    class on Personal Health Informatics.`,
+    balance between social and personal well-being.`,
     resultsText: `We developed high-fidelity functional prototypes on Figma and
-    conducted user testing. The feedback we received highlighted
+    conducted user testing. The feedback we received same-size
     discrepancies in users' mental models, and we devised solutions to
     bridge these gaps.`,
   },
@@ -115,35 +114,35 @@ const data = {
     title: "Designing for Efficiency",
     keyText: "I led end-to-end B2B dashboard projects.",
     icon: "./assets/projects/JOU.svg",
-    highlightedIcon: "./assets/projects-highlighted/JOU.svg",
+    sameSizeIcon: "./assets/projects-same-size/JOU.svg",
     imageAlt: "Description 2",
     problemText: `This is an ongoing project where I am leading the UX initiatives for a
-          climate-focused startup. Here,  I lead full-stack projects from client interviews to dashboard designs and the implementation of those designs.`,
+          climate-focused startup.`,
     resultsText: null,
   },
   [projectNames.DHY]: {
-    title: "Expanding the Market Reach of Dhyana",
+    title: "Expanding the Market Reach",
     keyText: "Our ring sales tripled times in one year.",
     icon: "./assets/projects/DHY.svg",
-    highlightedIcon: "./assets/projects-highlighted/DHY.svg",
+    sameSizeIcon: "./assets/projects-same-size/DHY.svg",
     imageAlt: "Description 1",
     problemText:
-      "Dhyana is a smart ring that leverages Heart Rate Variability (HRV) to evaluate the effectiveness of meditation sessions. Despite receiving positive feedback from users, the product's reach was predominantly among early adopters. Our objective was to boost sales by targeting a broader market.",
+      "Our objective was to boost sales of our wearable by targeting a broader market.",
     resultsText: `The user base surged from fewer than 500 to over 50,000 and sales tripled within a
     year. In addition, the website redesign led to approximately 50% reduction in the
     bounce rate.
     `,
+    mainImage: "./assets/images-on-pages/dhyana/cover.png",
   },
   [projectNames.UI]: {
     title: "A multitouch interactive tool",
     keyText: `Developed using JS,
     the tool enables users to interact via pointers`,
     icon: "./assets/projects/UI.svg",
-    highlightedIcon: "./assets/projects-highlighted/UI.svg",
+    sameSizeIcon: "./assets/projects-same-size/UI.svg",
     imageAlt: "Description 2",
     problemText: `In this project, we aimed to introduce a tool for exploring
-    multivariate data, capitalizing on the increased availability and
-    affordability of touch-based systems.`,
+    multivariate data.`,
     resultsText: `We designed and implemented a multitouch interactive
     data-visualization tool targeted at middle-school students to
     facilitate data exploration practice. The tool enables users to interact via pointers, allowing for the use of
@@ -155,29 +154,26 @@ const data = {
     title: "Tracking sentiment shifts",
     keyText: "I achieved the third place at the global competition.",
     icon: "./assets/projects/TABLEAU.svg",
-    highlightedIcon: "./assets/projects-highlighted/TABLEAU.svg",
+    sameSizeIcon: "./assets/projects-same-size/TABLEAU.svg",
     imageAlt: "Description 2",
-    problemText: `The Tableau 2021 Student Iron Viz competition challenged
-    participants to tell a story using music data. The entries were
-    evaluated based on creativity, analysis, beauty/design, and
-    adherence to best practices.`,
+    problemText: `My winning entry to the Tableau 2021 Student Iron Viz competition to tell a story using music data.`,
     resultsText: `My data visualization secured third place among global entries.
     Utilizing Spotify metrics, Billboard songs, lyric keywords, and
     the NRCLex library, I crafted a dashboard that illustrated the
     shifting sentiments of people over time through their preferences
     in music.`,
     website: "https://nive21.github.io/data-driven-story/",
+    mainImage: "./assets/images-on-pages/music/ironviz-photo.png",
   },
   [projectNames.SCROLLY]: {
     title: "Data-driven scrollytelling",
     keyText: `The project was recognized as a top project in the class
     that year.`,
     icon: "./assets/projects/SCROLLY.svg",
-    highlightedIcon: "./assets/projects-highlighted/SCROLLY.svg",
+    sameSizeIcon: "./assets/projects-same-size/SCROLLY.svg",
     imageAlt: "Description 2",
     problemText: `The challenge was to create a scrollytelling website that
-    presented data on police violence in the US for my graduate class,
-    'Principles of Data Visualization'.`,
+    presented data on police violence in the US.`,
     resultsText: `Utilizing tools like Tableau, AfterEffects, and HTML/CSS, we
     developed a website that narrated a story through insights
     revealed and charts animated upon scrolling. The site also
@@ -188,28 +184,23 @@ const data = {
     title: "Gun discourse on Reddit",
     keyText: `The paper has been submitted to the CSCW 2024 conference.`,
     icon: "./assets/projects/REDDIT.svg",
-    highlightedIcon: "./assets/projects-highlighted/REDDIT.svg",
+    sameSizeIcon: "./assets/projects-same-size/REDDIT.svg",
     imageAlt: "Description 2",
-    problemText: `As part of Dr. Amy Bruckman's team, I delved into the discourse
-    surrounding guns on Reddit. Our goal was to understand the
-    motivations and impact of these online conversations, and identify
-    ways to promote to constructive intergroup interactions.`,
+    problemText: `Our goal was to understand the
+    motivations and impact of online conversations on guns.`,
     resultsText: `Our research paper was submitted to the Computer-Supported
     Cooperative Work & Social Computing (CSCW) 2024 conference and is
     currently undergoing review.`,
   },
   [projectNames.SPEAKER]: {
-    title: "Audio Stories to Remember the Departed",
+    title: "Immersive Audio Stories",
     keyText: `Our exploration led us to the potential of using audio stories to
     memorialize the departed`,
     icon: "./assets/projects/SPEAKER.svg",
-    highlightedIcon: "./assets/projects-highlighted/SPEAKER.svg",
+    sameSizeIcon: "./assets/projects-same-size/SPEAKER.svg",
     imageAlt: "Description 2",
     problemText: `Our objective was to explore the problem and
-    solution space around the theme ‘Remembering the Departed’. We
-    gathered survey data and iterated on prototypes, evaluated them
-    against a set of usability criteria, selected the most promising
-    prototype, and conducted a Wizard-of-Oz test.`,
+    solution space around the theme ‘Remembering the Departed’.`,
     resultsText: `Our exploration led us to the potential of using audio stories to
     memorialize the departed. The Wizard-of-Oz prototype testing
     resulted in several design implications.`,
@@ -219,25 +210,23 @@ const data = {
     keyText: `Based on insights gained from the five tasks,
     I formulated three "how-might-we" insights.`,
     icon: "./assets/projects/FOOD.svg",
-    highlightedIcon: "./assets/projects-highlighted/FOOD.svg",
+    sameSizeIcon: "./assets/projects-same-size/FOOD.svg",
     imageAlt: "Description 2",
-    problemText: `As part of a school project for the Qualitative Research Methods
-    course, I conducted a participatory design study with three
-    classmates acting as the participants. My aim was to explore how
-    we could design food experiences tailored to students on campus. I
-    had 30 minutes to complete the session.`,
+    problemText: `My aim was to explore how
+    we could design food experiences for students through a participation study.`,
     resultsText: `Based on insights gained from the five tasks centered around food,
     I formulated three "how-might-we" insights.`,
   },
 };
 
 // Function to create a project section
-function createProjectSection(project, projectName) {
-  const section = document.createElement("div");
-  section.className = "section";
-
+function createProjectSection(projectName, smallProject = true) {
+  const project = data[projectName];
   const card = document.createElement("div");
-  card.className = "card";
+  card.className = `card ${smallProject ? "card-small" : "card-large"}`;
+  card.addEventListener("click", () => {
+    window.open(projectPages[projectName], "_blank");
+  });
 
   const content = document.createElement("div");
   content.className = "content-container";
@@ -249,8 +238,9 @@ function createProjectSection(project, projectName) {
   titleContainer.className = "title-container";
 
   const img = document.createElement("img");
-  img.src = project.highlightedIcon;
-  img.alt = project.imageAlt;
+  img.src = project.sameSizeIcon;
+  img.alt = projectName;
+  img.className = "card-icon";
 
   const title = document.createElement("h1");
   title.textContent = project?.title ?? "";
@@ -258,21 +248,12 @@ function createProjectSection(project, projectName) {
   const contextContainer = document.createElement("div");
   contextContainer.className = "context-container";
 
-  const problemHeading = document.createElement("h3");
-  problemHeading.textContent = "Context";
-
   const problemText = document.createElement("p");
   problemText.textContent = project.problemText;
 
-  const keyText = document.createElement("h2");
-  keyText.textContent = project?.keyText ?? "";
-
   const projectPage = document.createElement("button");
   projectPage.className = "page-button";
-  projectPage.textContent = "READ MORE";
-  projectPage.addEventListener("click", () => {
-    window.open(projectPages[projectName], "_blank");
-  });
+  projectPage.textContent = "READ MORE ->";
 
   card.appendChild(content);
   content.appendChild(topContainer);
@@ -280,12 +261,26 @@ function createProjectSection(project, projectName) {
   titleContainer.appendChild(img);
   titleContainer.appendChild(title);
   topContainer.appendChild(contextContainer);
-  contextContainer.appendChild(problemHeading);
   contextContainer.appendChild(problemText);
-  contextContainer.appendChild(keyText);
   content.appendChild(projectPage);
 
-  section.appendChild(card);
+  if (!smallProject) {
+    const mainImage = document.createElement("img");
+    mainImage.className = "main-img";
+    mainImage.src = project?.mainImage;
+    mainImage.alt = projectName;
+    card.appendChild(mainImage);
+  }
+
+  return card;
+}
+
+function createSideBySideProjects(project1, project2) {
+  const section = document.createElement("div");
+  section.className = "sbs-section";
+
+  section.appendChild(createProjectSection(project1));
+  section.appendChild(createProjectSection(project2));
 
   return section;
 }
@@ -403,12 +398,13 @@ async function createSphere(containerElement, projectList) {
   // Open details on a new page when clicked
   Events.on(mouseConstraint, "mousedown", (event) => {
     if (event?.source?.body?.label) {
-      const projectIndex = Object.values(cardOpened).indexOf(
-        event.source.body.label
-      );
-      const yPosition = (projectIndex + 1) * window.innerHeight;
-      console.log("y ", yPosition);
-      window.scrollTo(0, yPosition);
+      // Scrolls to that height
+      // const projectIndex = Object.values(cardOpened).indexOf(
+      //   event.source.body.label
+      // );
+      // const yPosition = (projectIndex + 1) * window.innerHeight;
+      // window.scrollTo(0, yPosition);
+      window.open(projectPages[event.source.body.label], "_blank");
     }
 
     // Manually trigger the end of the interaction
@@ -465,7 +461,7 @@ async function createSphere(containerElement, projectList) {
     if (pageScrolled) {
       for (const [index, item] of stack.entries()) {
         if (item.label === currentlyOpenCard) {
-          item.render.sprite.texture = data?.[item.label]?.highlightedIcon;
+          item.render.sprite.texture = data?.[item.label]?.sameSizeIcon;
         } else {
           item.render.sprite.texture = data?.[item.label]?.icon;
         }
@@ -485,10 +481,17 @@ window.addEventListener("load", async function () {
   // The project list container
   const projectsList = document.querySelector(".projects-list");
 
-  Object.values(cardOpened).forEach((projectName) => {
-    const projectSection = createProjectSection(data[projectName], projectName);
-    projectsList.appendChild(projectSection);
-  });
+  projectsList.appendChild(createProjectSection(projectNames.DHY, false));
+  projectsList.appendChild(
+    createSideBySideProjects(projectNames.SCROLLY, projectNames.REDDIT)
+  );
+  projectsList.appendChild(createProjectSection(projectNames.TABLEAU, false));
+  projectsList.appendChild(
+    createSideBySideProjects(projectNames.CIRCLE, projectNames.SPEAKER)
+  );
+  projectsList.appendChild(
+    createSideBySideProjects(projectNames.UI, projectNames.FOOD)
+  );
 });
 
 const arrowButton = document.getElementById("arrow");
@@ -500,20 +503,25 @@ arrowButton.addEventListener("click", function (_) {
 window.addEventListener(
   "scroll",
   function (_) {
-    const cardNum = this.scrollY / this.window.innerHeight;
-    const cardIndex = Math.floor(cardNum);
+    // const cardNum = this.scrollY / this.window.innerHeight;
+    // const cardIndex = Math.floor(cardNum);
 
-    if (currentlyOpenCard !== cardOpened[cardIndex]) {
-      pageScrolled = true;
-      currentlyOpenCard = cardOpened[cardIndex];
+    // if (currentlyOpenCard !== cardOpened[cardIndex]) {
+    //   pageScrolled = true;
+    //   currentlyOpenCard = cardOpened[cardIndex];
 
-      if (cardIndex === totalProjects) {
-        arrowButton.className = "hide-arrow";
-      } else {
-        arrowButton.classList.remove("hide-arrow");
-      }
+    //   if (cardIndex === totalProjects) {
+    //     arrowButton.className = "hide-arrow";
+    //   } else {
+    //     arrowButton.classList.remove("hide-arrow");
+    //   }
+    // } else {
+    //   pageScrolled = false;
+    // }
+    if (this.scrollY > 50) {
+      arrowButton.className = "hide-arrow";
     } else {
-      pageScrolled = false;
+      arrowButton.classList.remove("hide-arrow");
     }
   },
   false
